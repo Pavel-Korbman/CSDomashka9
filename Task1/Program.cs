d138a69bd3e1230a1cdb8493876c808ac585b1df
Pavel <pavel@b2profit.ru>
2023-11-26 18:40:18 +0300

    Решил первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string PrintNumbers(int start)
+{
+if (start == 1) return start.ToString();
+else return start+" "+ PrintNumbers(start-1);
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number<=0) Console.WriteLine("Число должно быть положительным");
+else Console.WriteLine(PrintNumbers(number));
